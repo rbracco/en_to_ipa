0b7916f3dacc9e3caaebc6cbca39b33e1fd97fd7
Robert Bracco <robertbracco1@gmail.com>
2021-09-14 16:32:37 -0400

    Remove 'keep_spaces' parameter

--- en_to_ipa/en_to_ipa.py
+++ en_to_ipa/en_to_ipa.py
@@ -13,16 +13,26 @@ __all__ = ["convert_label_to_phones", "arpa_to_ipa"]
 
 
 def convert_label_to_phones(
-    label, ipa=True, keep_spaces=True, as_list=True, raise_oov=True, warn_oov=True
+    label: str,
+    ipa: bool = True,
+    as_list: bool = True,
+    raise_oov: bool = True,
+    warn_oov: bool = True,
 ):
-    """Convert an entire label from graphemes to phonemes(IPA)"""
+    """Convert an entire label from graphemes to phonemes(IPA)
+
+    label: str - English grapheme label to be converted to IPA
+    ipa: bool - If True convert to IPA, else convert to ARPA
+    as_list: bool - If True return a list of characters else join them as a string
+    raise_oov: bool - If true raise an error if the word isn't found in CMUDict, if False ignore
+    warn_oov: bool - If true issue a warnings.Warning if the word isn't found in CMUDict
+    """
     phones = []
     label = _clean_label(label, permitted_punctuation=PERMITTED_PUNCTUATION)
     for word in label.split(" "):
         phones.extend(_convert_word_to_phones(word, ipa, raise_oov, warn_oov))
-        if keep_spaces:
-            phones.extend(" ")
-    phones = phones[:-1] if keep_spaces else phones
+        phones.extend(" ")
+    phones = phones[:-1]
     return phones if as_list else "".join(phones)
 
 
